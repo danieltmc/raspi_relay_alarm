e921d90c91e74fd03a9028a3eb66bdb9b252c5ea
danieltmc <me@danieltmc.com>
2015-11-23 14:39:48 -0500

    Update light.py
    Used some Object-Oriented Programming to simplify the addition of alarms.
    
    Alarms still need to be completed.
    
    Code needs to be tested.

--- light.py
+++ light.py
@@ -1,57 +1,87 @@
-#!/usr/bin/python
 import RPi.GPIO as GPIO
 import time
 
-# Set up GPIO pins
 GPIO.setmode(GPIO.BCM)
 
-# pinList = [relay1,relay2,relay3,relay4]
-pinList = [2, 3, 4, 17]
+pinList = [2,3,4,17]
 
-for i in pinList: 
-    # setup each relay
-    GPIO.setup(i, GPIO.OUT)
-    # set each relay to off 
-    GPIO.output(i, GPIO.HIGH)
+for i in pinList:
+	GPIO.setup(i,GPIO.OUT)
+	GPIO.output(i,GPIO.HIGH)
+
+int_list = ['1','2','3','4','5','6','7','8','9','0']
+
+class Light:
+	def __init__(self):
+		self.status = False
+		self.alarms = []
+	def __str__(self):
+		return('Light status: ' + str(self.status) + '\n' + 'Current alarms: ' + str(self.alarms) + '\n')
+	def add_alarm(self, alarm_time, ampm,alarm_day,recurring):
+		self.alarms.append(alarm_time)
+		#this_year = time.localtime().tm_year
+		#this_month = time.localtime().tm_mon
+		#if alarm_day == 'today':
+		#	this_day = time.localtime().tm_mday
+		#elif alarm_day == 'tomorrow':
+		#	this_day = time.localtime().tm_mday+1
+		#this_hour = int(alarm_time[0:alarm_time.index(':')])
+		#this_minute = int(alarm_time[alarm_time.index(':')+1:])
+		#this_second = 0
+		#need to set recurring
+		
+	def clear_alarms(self):
+		self.alarms = []
+	def light_on(self):
+		try:
+			GPIO.output(2,GPIO.LOW)
+			self.status = True
+		except keyboardInterrupt:
+			print('KeyboardInterrupt')
+			GPIO.cleanup()
+	def light_off(self):
+		try:
+			GPIO.output(2,GPIO.HIGH)
+			self.status = False
+		except KeyboardInterrupt:
+			print('KeyboardInterrupt')
+			GPIO.cleanup()
 
 def light():
-  # False == light off, True == light on
-  current_status = False
+	raspi = Light()
+	print(str(raspi))
 	while True:
-		# take the user input as a string and convert to lowercase for easier use
-		user_input = str(input('On or off?\n')).lower()
-		# if the word 'on' appears in the input, turn the light on
-		if 'on' in user_input:
-		  try:
-	      GPIO.output(2, GPIO.LOW)
-	      current_status = True
-      except KeyboardInterrupt:
-	      print('KeyboardInterrupt')
-	      GPIO.cleanup()
-		# if the word 'off' appears in the input, turn the light off
-		elif 'off' in user_input:
-		  try:
-	      GPIO.output(2, GPIO.HIGH)
-	      current_status = False
-      except KeyboardInterrupt:
-	      print('KeyboardInterrupt')
-	      GPIO.cleanup()
-		# if the word 'stop' appears in the input, end the program
-		elif 'stop' in user_input:
-			print('Stopping')
-			# return statement ends the function
-			return
-		else:
-			# if an unrecognized word appears in the input, let the user know
-			print('Unrecognized command')
-# start the program
-light()
-# cleanup the GPIO pins after the program ends
-GPIO.cleanup()
+		try:
+			user_input = str(input('On or off?\n')).lower()
+			if 'on' in user_input:
+				raspi.light_on()
+			elif 'off' in user_input:
+				raspi.light_off()
+			elif ('set' in user_input) and ('alarm' in user_input):
+				colon_at = user_input.index(':')
+				if user_input[colon_at-2] in int_list:
+					alarm_time = user_input[colon_at-2:colon_at+3]
+				else:
+					alarm_time = user_input[colon_at-1:colon_at+3]
+				ampm = 'am'
+				if ('pm' in user_input) or ('p.m' in user_input):
+					ampm = 'pm'
+				if 'today' in user_input:
+					alarm_day = 'today'
+				if ('recurring' in user_input) or ('repeat' in user_input):
+					recurring = True
+				else:
+					recurring = False
+				else:
+					alarm_day = 'tomorrow'
+				raspi.add_alarm(alarm_time,ampm,alarm_day,recurring)
+			elif 'stop' in user_input:
+				print('Stopping')
+				return
+			else:
+				print('Unrecognized command')
+		except KeyboardInterrupt:
+			print('KeyboardInterrupt')
+			GPIO.cleanup()
 
-# try/except block for handling keyboard interrupt
-# try:
-# 	GPIO.output(*pin_number*, GPIO.LOW)
-# except KeyboardInterrupt:
-# 	print('KeyboardInterrupt')
-# 	GPIO.cleanup()
+light()
